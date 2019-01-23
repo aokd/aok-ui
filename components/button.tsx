883dcd0da57b3b94f0302eae7e53c1b42dcc3d85
Chr15t0pher <chr1st0pher5henxy@gmail.com>
2019-01-23 22:48:31 +0800

    feat: markdown docs

---
+++ components/button.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+
+// tslint:disable
+export interface IAppProps {}
+
+export class Button extends React.Component<IAppProps, any> {
+  public render() {
+    return <div>按钮</div>;
+  }
+}
