69d6321aeca0aa205a9e22bbc2582ba24cfb6f47
Chr15t0pher <chr1st0pher5henxy@gmail.com>
2019-03-20 11:31:45 +0800

    feat: add renderAPI

--- site/global.d.ts
+++ site/global.d.ts
@@ -1,9 +1,14 @@
+declare module '*.styl' {
+  const classes: { [key: string]: string }
+  export default classes
+}
+
 declare module '*.yaml' {
-  const content: any;
-  export default content;
+  const content: any
+  export default content
 }
 
 declare module '*.md' {
-  const content: any;
-  export default content;
+  const content: any
+  export default content
 }
